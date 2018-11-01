9020bc4f11cfc19917ac71478a1efc50364ec8f6
Talitamota <talitamotan@gmail.com>
2018-11-01 15:37:20 -0300

    configura media_url e adiciona static no arquivo urls

--- projeto_desafio/settings.py
+++ projeto_desafio/settings.py
@@ -70,7 +70,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projeto_desafio.wsgi.application'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
